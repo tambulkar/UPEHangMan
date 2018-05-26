3f2139563544b94391b8d2b3c1c2c194ec50bb26
tambulkar <tanish.ambulkar99@gmail.com>
2018-05-25 17:07:45 -0700

    Removed Extra Dictionary

--- hangman.py
+++ hangman.py
@@ -4,8 +4,8 @@ import operator
 import time
 
 dict = {}
-for line in open('./dictionary2.txt','r').readlines():
-    wordList = open('./dictionary2.txt','r').readlines()
+for line in open('./dictionary.txt','r').readlines():
+    wordList = open('./dictionary.txt','r').readlines()
     if((len(line)-1) in dict.keys()):
         dict[len(line)-1].append(line[:-1])
     else:
@@ -40,17 +40,20 @@ def findMostComLetter(candidates):
                     letterCount[letter] = letterCount[letter] + 1
                 else:
                     letterCount[letter] = 1
+
     #If word doesn't exist, guess next most common letter
     if(len(letterCount) is 0):
         letter = freqLetters[0]
         return letter
+
     #Guess letter with highest count
     return max(letterCount.items(), key=operator.itemgetter(1))[0]
 
 
-for i in range(1):
+for i in range(100):
     #Reset lists at beginning of every game
     resetLists()
+
     #Start Game
     r = requests.get("http://upe.42069.fun/7GBQY")
     print(r.json())
@@ -85,21 +88,26 @@ for i in range(1):
         #Find longest word that hasn't been solved and find possible candidates
         longestWord = max(words, key=len)
         candidates = list(findCandidates(longestWord, dict[len(longestWord)]))
+
         #Guess most common letters in the candidates
         randLetter = findMostComLetter(candidates)
+
         #Remove guessed letter
         alphabet.remove(randLetter)
         freqLetters.remove(randLetter)
+
         #Make Guess request
         r = requests.post('http://upe.42069.fun/7GBQY', data = {'guess': randLetter})
         time.sleep(0.2)
         print(r.json())
+
     #Write new words to dictionary
     if(r.json()["status"] == "DEAD"):
-        f = open('./dictionary2.txt', 'a+')
+        f = open('./dictionary.txt', 'a+')
         lyrics = r.json()["lyrics"].split(" ")
         for word in lyrics:
             editedWord = re.sub('[^a-z]', '', word)
             f.write(editedWord + "\n")
         f.close()
+
     time.sleep(1)
